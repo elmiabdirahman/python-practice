3019eed1df216ec69a9b123f23ebbe50da82f8a4
Elmi Elmi <elmielmi522@gmail.com>
2022-01-17 00:45:54 -0600

    filter

--- functions.py
+++ functions.py
@@ -1,4 +1,38 @@
-def my_func(param1='default'):
-    print("my first python function")
+from unittest import result
 
-my_func()
+
+# def addnum(num1, num2):
+#     if type(num1)==type(num2)==type(10):
+#         return num1+num2
+#     else:
+#         return "sorry, i need integers!"
+
+# result = addnum("2","3")
+# print(result)
+
+#######lambda expresio
+
+######filter
+
+# def add_numbers(n1, n2):
+#     result = int(n1) + int(n2)
+#     print("the sumis", result)
+
+# number1 = input("what is you first number: ")
+# number2 = input("what is you second number: ")
+# add_numbers(number1, number2)
+
+# def add_numbers(n1, n2):
+#     result = int(n1) + int(n2)
+#     return result
+
+# number1 = input("what is you first number: ")
+# number2 = input("what is you second number: ")
+# result = add_numbers(number1, number2)
+# print("the sum is: ", result)
+
+marks = [55, 64, 75, 80, 34]
+
+length = len(marks)
+print("length is", length)
+
